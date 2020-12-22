ba29dd2f55c7f1d9121b33812f4bda87701cfde3
Johannes Berthel <j.berthel@codeblick.de>
2020-12-22 17:22:47 +0100

    changed namespace

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-    namespace: 'node-etl-components',
+    namespace: 'node-etl-ui',
     plugins: [
         sass({
             injectGlobalPaths: ['src/globals/variables.scss']
